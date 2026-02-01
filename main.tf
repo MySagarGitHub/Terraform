403750c240888ab8de7050c6a90ebca0c61ea760
MySagarGithub <Pandaysagar2004@gmail.com>
2026-02-01 19:49:39 +0545

    create vpc:Terraform

--- main.tf
+++ main.tf
@@ -8,24 +8,24 @@ provider "aws" {
   region = "us-east-1"
 }
 
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners      = ["amazon"]
+# data "aws_ami" "amazon_linux" {
+#   most_recent = true
+#   owners      = ["amazon"]
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-}
-resource "aws_instance" "my_ec2" {
-  ami           = data.aws_ami.amazon_linux.id   # Amazon Linux 2 (us-east-1)
-  instance_type = var.instance_type
+#   filter {
+#     name   = "name"
+#     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+#   }
+# }
+# resource "aws_instance" "my_ec2" {
+#   ami           = data.aws_ami.amazon_linux.id   # Amazon Linux 2 (us-east-1)
+#   instance_type = var.instance_type
 
-  tags = {
-    Name = "EC2-${terraform.workspace}"
-    environment = terraform.workspace
-  }
-}
+#   tags = {
+#     Name = "EC2-${terraform.workspace}"
+#     environment = terraform.workspace
+#   }
+# }
 
 # resource "aws_s3_bucket" "my_bucket" {
 #   bucket = var.bucket_name
@@ -54,3 +54,21 @@ resource "aws_instance" "my_ec2" {
 #   name          = "my-module-ec2"
   
 # }
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
